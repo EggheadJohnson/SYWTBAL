54da2074652424e2d34d7d35d069ce5267f67d39
Paul Johnson <PaulE.Johnson@gmail.com>
2018-09-26 16:19:54 -0600

    Lesson 02

--- iridium/src/vm.rs
+++ iridium/src/vm.rs
@@ -1,12 +1,43 @@
+use instruction::Opcode;
+
 pub struct VM {
-    registers: [i32; 32]
+    registers: [i32; 32],
+    pc: usize,
+    program: Vec<u8>
 
 }
 
 impl VM {
     pub fn new() -> VM {
         VM {
-            registers: [0; 32]
+            registers: [0; 32],
+            program: vec![],
+            pc: 0,
+        }
+    }
+
+    fn decode_opcode(self: &mut Self) -> Opcode {
+        let opcode = Opcode::from(self.program[self.pc]);
+        self.pc += 1;
+        return opcode;
+    }
+    pub fn run(&mut self) {
+        loop {
+            if self.pc >= self.program.len() {
+                break;
+            }
+
+            match self.decode_opcode() {
+                Opcode::HLT => {
+                    println!("HLT encountered!");
+                    return;
+
+                }
+                _ => {
+                    println!("Unrecognized command! Terminating!");
+                    return;
+                }
+            }
         }
     }
 }
@@ -19,5 +50,23 @@ mod tests {
     fn test_create_vm() {
         let test_vm = VM::new();
         assert_eq!(test_vm.registers[0], 0)
+    }
+
+    #[test]
+    fn test_opcode_hlt() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![0,0,0,0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_igl() {
+        let mut test_vm = VM::new();
+        let test_bytes = vec![200,0,0,0];
+        test_vm.program = test_bytes;
+        test_vm.run();
+        assert_eq!(test_vm.pc, 1);
     }
 }
