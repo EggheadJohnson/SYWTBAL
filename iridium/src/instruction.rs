0079a6dc480a242679fb85091ab57004854acaf4
Paul Johnson <PaulE.Johnson@gmail.com>
2018-10-04 16:23:06 -0600

    Lesson 3: Basic math

--- iridium/src/instruction.rs
+++ iridium/src/instruction.rs
@@ -1,6 +1,11 @@
 #[derive(Debug,PartialEq)]
 pub enum Opcode {
     HLT,
+    LOAD,
+    ADD,
+    SUB,
+    MUL,
+    DIV,
     IGL
 }
 
@@ -21,6 +26,11 @@ impl From<u8> for Opcode {
     fn from(v: u8) -> Self {
         match v {
             0 => Opcode::HLT,
+            1 => Opcode::LOAD,
+            2 => Opcode::ADD,
+            3 => Opcode::SUB,
+            4 => Opcode::MUL,
+            5 => Opcode::DIV,
             _ => Opcode::IGL
         }
     }
--- iridium/src/vm.rs
+++ iridium/src/vm.rs
@@ -3,7 +3,8 @@ use instruction::Opcode;
 pub struct VM {
     registers: [i32; 32],
     pc: usize,
-    program: Vec<u8>
+    program: Vec<u8>,
+    remainder: u32
 
 }
 
@@ -13,6 +14,7 @@ impl VM {
             registers: [0; 32],
             program: vec![],
             pc: 0,
+            remainder: 0
         }
     }
 
@@ -22,29 +24,81 @@ impl VM {
         return opcode;
     }
     pub fn run(&mut self) {
-        loop {
-            if self.pc >= self.program.len() {
-                break;
-            }
+        let mut is_done = false;
+        while !is_done {
+            is_done = self.execute_instruction();
+        }
+    }
 
-            match self.decode_opcode() {
-                Opcode::HLT => {
-                    println!("HLT encountered!");
-                    return;
+    pub fn run_once(&mut self) {
+        self.execute_instruction();
+    }
 
-                }
-                _ => {
-                    println!("Unrecognized command! Terminating!");
-                    return;
-                }
+    fn execute_instruction(&mut self) -> bool{
+        if self.pc >= self.program.len() {
+            return true;
+        }
+
+        match self.decode_opcode() {
+            Opcode::HLT => {
+                println!("HLT encountered!");
+                return true;
+
+            },
+            Opcode::LOAD => {
+                let register = self.next_8_bits() as usize;
+                let number = self.next_16_bits() as u16;
+                self.registers[register] = number as i32;
+            },
+            Opcode::ADD => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 + register2;
+            },
+            Opcode::SUB => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 - register2;
+            },
+            Opcode::MUL => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 * register2;
+            },
+            Opcode::DIV => {
+                let register1 = self.registers[self.next_8_bits() as usize];
+                let register2 = self.registers[self.next_8_bits() as usize];
+                self.registers[self.next_8_bits() as usize] = register1 / register2;
+                self.remainder = (register1 % register2) as u32;
+            },
+            _ => {
+                println!("Unrecognized command! Terminating!");
+                return true;
             }
         }
+
+        false
+    }
+
+    fn next_8_bits(&mut self) -> u8 {
+        let result = self.program[self.pc];
+        self.pc += 1;
+        return result;
+    }
+    fn next_16_bits(&mut self) -> u16 {
+        let result = ((self.program[self.pc] as u16) << 8) | self.program[self.pc+1] as u16;
+        self.pc += 2;
+        return result;
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn get_test_vm() -> VM {
+        return VM::new();
+    }
 
     #[test]
     fn test_create_vm() {
@@ -68,5 +122,75 @@ mod tests {
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.pc, 1);
+    }
+
+    #[test]
+    fn test_opcode_load() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 1, 244];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 500);
+    }
+
+    #[test]
+    fn test_opcode_add() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 0, 10, 1, 1, 0, 20, 2, 0, 1, 2];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 10);
+        assert_eq!(test_vm.registers[1], 20);
+        assert_eq!(test_vm.registers[2], 30);
+    }
+
+    #[test]
+    fn test_opcode_sub_pos_result() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 0, 20, 1, 1, 0, 10, 3, 0, 1, 2];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 20);
+        assert_eq!(test_vm.registers[1], 10);
+        assert_eq!(test_vm.registers[2], 10);
+    }
+
+    #[test]
+    fn test_opcode_sub_neg_result() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 0, 10, 1, 1, 0, 20, 3, 0, 1, 2];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 10);
+        assert_eq!(test_vm.registers[1], 20);
+        assert_eq!(test_vm.registers[2], -10);
+    }
+
+    #[test]
+    fn test_opcode_mul() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 0, 10, 1, 1, 0, 20, 4, 0, 1, 2];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 10);
+        assert_eq!(test_vm.registers[1], 20);
+        assert_eq!(test_vm.registers[2], 200);
+    }
+
+    #[test]
+    fn test_opcode_div_no_remainder() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 0, 20, 1, 1, 0, 10, 5, 0, 1, 2];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 20);
+        assert_eq!(test_vm.registers[1], 10);
+        assert_eq!(test_vm.registers[2], 2);
+        assert_eq!(test_vm.remainder, 0);
+    }
+
+    #[test]
+    fn test_opcode_div_with_remainder() {
+        let mut test_vm = get_test_vm();
+        test_vm.program = vec![1, 0, 0, 10, 1, 1, 0, 3, 5, 0, 1, 2];
+        test_vm.run();
+        assert_eq!(test_vm.registers[0], 10);
+        assert_eq!(test_vm.registers[1], 3);
+        assert_eq!(test_vm.registers[2], 3);
+        assert_eq!(test_vm.remainder, 1);
     }
 }
